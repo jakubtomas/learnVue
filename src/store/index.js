f95a0c631288954b4dbc34a0a857ce2bf4eca1f8
tomasjakub <tomasjakub21@gmail.com>
2020-07-23 20:56:01 +0200

    add refs

--- src/store/index.js
+++ src/store/index.js
@@ -3,36 +3,53 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    score: 15
-  },
-  mutations: {
-    INCREMENT(state, value)
-    {
-      state.score += value
-    },
-    DECREMENT(state, value)
-    {
-      state.score-= value
-    }
-  },
-  actions: {
-    increment(context){
-      let rand = Math.floor(Math.random() *100)
-      if (rand < 15) {
-        console.log("oh no");
-      }else{
-        context.commit('INCREMENT', rand)
-      }
-    }
+const state = {
+  score: 15
+}
+
+const mutations = { // mutacie su operacie ktoreo meni state , nemalo by obsahovat vypočty prepočty iba jeden riadok
+  INCREMENT(state, value)
+  {
+    state.score += value
 
   },
-  getters: {
-    scoreButPretty(state)
+  DECREMENT(state, value = 1)
+  {
+    state.score -= value
+  }
+}
+ 
+const actions = {  // actions pises ked potrebujes nejaku vvypocet alebo upravu 
+  increment(context)
+  {
+    let rand = Math.floor(Math.random() * 100)
+    if (rand < 15)
     {
-      return state.score
+      console.log("oh no");
+    } else
+    {
+      context.commit('INCREMENT', rand)
     }
-  },
-  modules: {}
+  }
+}
+
+const getters = { // getters sa pouziva na vypisanie hodnoty  v kode 
+  scoreButPretty(state)
+  {
+    return state.score
+  }
+}
+
+const modules = {}
+
+// A Vuex instance is created by combining the state, mutations, actions,
+// and getters.
+// exportovanie hodnot
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters,
+  modules
+
 });
--- src/views/Vuex.vue
+++ src/views/Vuex.vue
@@ -1,30 +1,60 @@
 
 <template>
   <div id="apps">
-      <h1>{{name}}</h1>
-      <h2> {{$store.state.score}}</h2>
-      <h2> {{$store.getters.scoreButPretty}}</h2>
-      <p>
-        <button @click="$store.dispatch('increment')"> plus</button>
-        <button @click="$store.commit('DECREMENT')"> MINUS</button>
-      </p>
+    <h1>{{name}}</h1>
+    <h2>{{$store.state.score}}</h2>
+    <h2>{{$store.getters.scoreButPretty}}</h2>
+    <p>
+      <button @click="$store.dispatch('increment')">plus</button>
+      <button @click="$store.commit('DECREMENT')">MINUS</button>
+
+      <button v-on:click="readRefs">readRefs</button>
+    </p>
+    <a href="http://google.com"></a>
+    <a v-bind:href="website">this is the link on website</a>
+
+    <p v-html="webTag"></p>
+    <div>
+      <input type="text" ref="input" /> 
+      <p>ref sluzi na manipulovanie DOM ,</p>
+      <button v-on:click="readRefs">readRefs</button>
+    </div>
+    <h3>-----------------------------------</h3>
+    <!-- 
+      <h2>Hello this is for refs man!</h2>
+      <p>You have counted {{this.counter}} times</p>
+      <input type="text" ref="input" />
+      <button @click="submit">Add 1 to counter</button>
+    -->
   </div>
 </template>
  
 
 <script>
-
 export default {
   data: function() {
     return {
+      counter: 0,
       name: "Data from vuex ",
+      website: "http://google.com",
+      webTag: '<a href="http://google.com">link na google</a>',
       array: [],
       info: null
     };
   },
   mounted() {},
 
-  methods: {},
+  methods: {
+    submit() {
+      console.log("function submit");
+      this.counter++;
+      console.log(this.ref);
+    },
+    readRefs: function() {
+      console.log("run function");
+      console.log(this.$refs);
+    }
+  },
 
   created() {}
 };
